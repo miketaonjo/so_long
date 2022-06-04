98243b4280c62caf56bd38243f54eb5e64712119
miketaonjo <79211048+miketaonjo@users.noreply.github.com>
2022-06-04 11:28:16 +0200

    Update mix.c

--- mix.c
+++ mix.c
@@ -150,3 +150,88 @@ t_root	*root_init(char *filename)
 	texture_init(root);
     return (root);
 }
+
+void	game_init(t_root *root, char *filename)
+{
+	root->game = (t_game *)malloc(sizeof(t_game));
+	if (root->game == 0)
+		root_destroy(root, "game_init(): malloc()", errno);
+	root->game->map = 0;
+	root->game->coll = 0;
+	root->game->count_coll = 0;
+	root->game->count_exit = 0;
+	root->game->count_player = 0;
+	root->game->player_move = 0;
+	root->game->player_coll = 0;
+	root->game->player_up = 0;
+	root->game->player_down = 0;
+	root->game->player_left = 0;
+	root->game->player_right = 0;
+	map_init(root, filename);
+}
+
+//gnl map
+static void	file_parse(t_root *root, char **file, char buf[], int fd)
+{
+	char			*tmp;
+
+	tmp = ft_strjoin(*file, buf);
+	free(*file);
+	*file = tmp;
+	if (*file == 0)
+	{
+		close(fd);
+		root_destroy(root, "map_init(): ft_strjoin()", errno);
+	}
+}
+
+static void	file_read(t_root *root, char **file, char buf[], int fd)
+{
+	int				ret;
+
+	ret = 1;
+	while (ret != 0)
+	{
+		ret = read(fd, buf, 1024);
+		if (ret == -1)
+		{
+			free(*file);
+			close(fd);
+			root_destroy(root, "map_init(): read()", errno);
+		}
+		else
+		{
+			buf[ret] = 0;
+			file_parse(root, file, buf, fd);
+		}
+	}
+}
+
+static char	*file_init(t_root *root, int fd)
+{
+	char			*file;
+
+	file = ft_calloc(1, 1);
+	if (file == 0)
+	{
+		close(fd);
+		root_destroy(root, "map_init(): ft_calloc()", errno);
+	}
+	return (file);
+}
+
+void	map_init(t_root *root, char *filename)
+{
+	int				fd;
+	char			*file;
+	char			buf[1024 + 1];
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		root_destroy(root, filename, errno);
+	file = file_init(root, fd);
+	file_read(root, &file, buf, fd);
+	close(fd);
+	map_read(root, file);
+	free(file);
+}
